06a54975f6a5e23f720e31a0ef86c3f5e219ca76
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-12-14 15:33:18 +0100

    finito

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
-   $text: "Giove è il quinto pianeta del sistema solare in ordine di distanza dal Sole e il più grande di tutto il sistema planetario: la sua massa corrisponde a due volte e mezzo la somma di quelle di tutti gli altri pianeti messi insieme. È classificato, al pari di Saturno, Urano e Nettuno, come gigante gassoso (gli ultimi due si differenziano per essere classificati in tempi recenti come giganti ghiacciati).";
+   $text = "Giove è il quinto pianeta del sistema solare in ordine di distanza dal Sole e il più grande di tutto il sistema planetario: la sua massa corrisponde a due volte e mezzo la somma di quelle di tutti gli altri pianeti messi insieme. È classificato, al pari di Saturno, Urano e Nettuno, come gigante gassoso (gli ultimi due si differenziano per essere classificati in tempi recenti come giganti ghiacciati).";
+   $censured_word = isset($_GET['word']) ? $_GET['word'] : '';
+   $text_censured = str_replace($censured_word, '***', $text);
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +13,15 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo $text; ?></h1>
+    <form action="index.php" method="GET">
+        <label for="censure-word">Parola da censurare</label>
+        <input name="word" id="censure-word" type="text" placeholder="Inserisci una parola">
+        <button type="submit">Invia</button>
+    </form>
+    <hr>
+    <p><?php echo $text; ?></p>
     <h2>La lunghezza del testo è di:<?php echo strlen($text); ?></h2>
+    <hr>
+    <p><?php echo $text_censured; ?></p>
 </body>
 </html>
